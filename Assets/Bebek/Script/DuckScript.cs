672562f920919421f6133d7ddf3236193c81ec23
simsonrinekso <reyno33333@gmail.com>
2022-06-20 15:03:05 +0700

    [ADD] first

---
+++ Assets/Bebek/Script/DuckScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckScript : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public Transform dir;
+    Vector3 direction;
+    Rigidbody rigidbody;
+    public bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        // rigidbody.velocity = Vector3.up*jumpForce;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(jump){
+            jump = false;
+            direction = (dir.position-transform.position).normalized;
+            rigidbody.AddForce(direction*speed,ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Bebek/Script/DuckShooter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckShooter : MonoBehaviour
+{
+    public GameObject Duck;
+    public int intervalDuck;
+    public int currentLevel=0;
+    public int[] levelTarget;
+    public Transform[] positions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateDuck();
+    }
+    void GenerateDuck(){
+        StartCoroutine(Generate());
+    }
+    IEnumerator Generate(){
+        for (int i = 0; i < levelTarget[currentLevel]; i++)
+        {
+            int id = Mathf.FloorToInt(Random.Range(0,positions.Length));
+            Instantiate(Duck,positions[i].position, new Quaternion()).GetComponent<DuckScript>().jump = true;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
